7b64dcfa478a771ff08862fcff0ca3d05c136c77
pinkimasud <60094830+pinkimasud@users.noreply.github.com>
2020-06-01 11:06:33 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// owl carousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+    });
+  });
+//   fiterable gallery
+
+  $('.portfolio-item').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+$('.portfolio-menu ul li').click(function(){
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+        filter:selector
+    });
+    return  false;
+});
+$(document).ready(function() {
+var popup_btn = $('.popup-btn');
+popup_btn.magnificPopup({
+type : 'image',
+gallery : {
+    enabled : true
+}
+});
+});
+  
